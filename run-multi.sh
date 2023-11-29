18db2e35cecbbb1241c67e5695264904527a4e13
juan-angel <juan.angel.gascon@gmail.com>
2023-11-29 16:20:24 +0100

    feat: Scripts added
    Scripts to execute the classes has been added

--- run-multi.sh
+++ run-multi.sh
@@ -26,7 +26,7 @@ else
 		;;
 	esac
 	
-	java -XX:+Use${GC}GC -XX:+PrintCommandLineFlags -cp target com.ing.jangel.garbagecollectors.MainSingleThread
+	java -XX:+Use${GC}GC -XX:+PrintCommandLineFlags -cp target com.ing.jangel.garbagecollectors.MainMultiThread
 	
 	if [ $? -ne 0 ]; then
     	echo "java is not present in your path"
---
+++ run.bat
@@ -0,0 +1,26 @@
+@echo off
+
+IF "%#%" EQU "0" (
+  echo "Please, specify the number of the GC you want to use as argument:"
+  echo "1) Serial"
+  echo "2) Parallel"
+  echo "3) G1"
+  echo "4) Z"
+) ELSE (
+  IF "%~1"=="1" (
+    SET "GC=Serial"
+  ) ELSE IF "%~1"=="2" (
+    SET "GC=Parallel"
+  ) ELSE IF "%~1"=="3" (
+    SET "GC=G1"
+  ) ELSE IF "%~1"=="4" (
+    SET "GC=Z"
+  ) ELSE (
+    echo "Unsupported option %~1"
+    exit
+  )
+  java "-XX:+Use%GC%GC" "-XX:+PrintCommandLineFlags" "-cp" "target" "com.ing.jangel.garbagecollectors.MainSingleThread"
+  IF "%?%" NEQ "0" (
+    echo "java is not present in your path"
+  )
+)
