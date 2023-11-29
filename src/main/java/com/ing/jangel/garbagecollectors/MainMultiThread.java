d8ce054fa13a8fc2833bdac8e8d0d83a73e46f2b
juan-angel <juan.angel.gascon@gmail.com>
2023-11-29 16:20:56 +0100

    feat: Classes added
    Classes to perform the work in a single or a multhreading env. have been added

---
+++ src/main/java/com/ing/jangel/garbagecollectors/MainMultiThread.java
@@ -0,0 +1,42 @@
+package com.ing.jangel.garbagecollectors;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class MainMultiThread {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Initializing");
+		Thread.sleep(15000);
+		var executorService = Executors.newFixedThreadPool(5);
+
+		var d = System.currentTimeMillis();
+
+		IntStream.iterate(0, i -> i < 10, i -> ++i).forEach(i -> executorService.execute(new MemoryConsumer(i)));
+		executorService.shutdown();
+	    executorService.awaitTermination(1000, TimeUnit.SECONDS);
+
+		System.out.println("Time: " + (System.currentTimeMillis() - d) + "ms.");
+	}
+
+	private static class MemoryConsumer implements Runnable {
+		private int from;
+
+		public MemoryConsumer(int from) {
+			this.from = from;
+		}
+
+		@Override
+		public void run() {
+			IntStream.iterate(from * 100, i -> i < (from * 100) + 100, i -> ++i).forEach(i -> {
+				var x = new byte[1024 * 1024 * 200]; // Allocate 100MB
+				System.out.println("iteration=> " + (i + 1));
+
+				if ((i + 1) % 100 == 0) { // Suggest GC to run every 100 iterations
+					System.gc();
+				}
+			});
+		}
+	}
+}
--- src/main/java/com/ing/jangel/garbagecollectors/MainSingleThread.java
+++ src/main/java/com/ing/jangel/garbagecollectors/MainSingleThread.java
@@ -2,7 +2,7 @@ package com.ing.jangel.garbagecollectors;
 
 import java.util.stream.IntStream;
 
-public class Main {
+public class MainSingleThread {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Initializing");
@@ -10,8 +10,8 @@ public class Main {
 
 		var d = System.currentTimeMillis();
 
-		IntStream.iterate(0, i -> ++i).limit(500).forEach(i -> {
-			var x = new byte[1024 * 1024 * 100]; // Allocate 100MB
+		IntStream.iterate(0, i -> ++i).limit(1_000).forEach(i -> {
+			var x = new byte[1024 * 1024 * 200]; // Allocate 100MB
 			System.out.println("iteration=> " + (i + 1));
 
 			if ((i + 1) % 100 == 0) { // Suggest GC to run every 100 iterations
@@ -19,7 +19,7 @@ public class Main {
 			}
 		});
 
-		System.out.println("Time: " + (System.currentTimeMillis() - d));
+		System.out.println("Time: " + (System.currentTimeMillis() - d) + "ms.");
 	}
 
 }
